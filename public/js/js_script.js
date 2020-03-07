433d3b44ec3c1a369bf167d063c2ab760d78dbec
Bela7171 <benny.lam.7171@student.uu.se>
2020-03-07 13:08:43 +0100

    Task 4-10 completed

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -3,7 +3,6 @@ function menuItem(name, allergy, kCal, img) {
     this.allergy = allergy;
     this.calorie = kCal;
     this.img = img;
-    // use: getElementById('make-image').src="/path/to/images" + selected_text + ".jpg" ?
     this.info = function() {
     	let burgerInf = new Array(this.allergy, this.calorie);
 		return burgerInf;
@@ -14,88 +13,29 @@ function menuItem(name, allergy, kCal, img) {
 		return img;
     }
   }
-/*
 
-  function inputer(burgerArray) {
-  	for (var i = 0; i<burgerArray.length; i++){
-  		var newDiv = document.createElement("div");
-  		let burgerPic = burgerArray[i].onload();
-  		burgerPic.height = "300";
-  		burgerPic.width = "300";
-  		document.getElementById(i+1).appendChild(burgerPic);
-
-  		let note = burgerArray[i].info();
-  		for(var j = 0; j<note.length; j++){
-  		var x = document.createElement("LI");
-  		var t = document.createTextNode( note[j]);
-  		newDiv.appendChild(x).appendChild(t);
-  		document.getElementById(i+1).appendChild(x);
-  		}
-  	}
+ 
+/* EventListener
+  let myButton = document.getElementById('confirmBtn');
+  myButton.addEventListener("click", myButton.onclick);
+  myButton.onclick = function () {
+  console.log("Button clicked!");
+  let fullName = document.getElementById('firstandlastname').value;
+  let emailAdd = document.getElementById('email').value;
+  let street = document.getElementById('street').value;
+  let number = document.getElementById('number').value;
+  function displayRadioValue() { 
+            var ele = document.getElementsByName('gender'); 
+            var counter = 0;
+            for(i = 0; i < ele.length; i++) { 
+                if(ele[i].checked)   
+                  counter++;
+              }
+              return ele[counter].value;
+        }   
+  let gender = displayRadioValue();         
+  let result = new Array(fullName, emailAdd, street, number,gender);
+  return console.log(result);
   }
-
-let firstBurger = new menuItem('Tasty', 'gluten', 550, "https://www.seriouseats.com/recipes/images/2015/07/20150727-teriyaki-burger-recipe-15-1500x1125.jpg");  
-let secondBurger = new menuItem('TheDestroyer', 'lactose', 650, "https://www.butchers-sundries.com/ekmps/shops/trunetpackaging/images/premium-chilli-burger-mix-500g-10629-p.jpeg");
-let thirdBurger = new menuItem('goNature', 'vegan', 600, "https://www.thespruceeats.com/thmb/UAh3Lv71pRikSXvICqkv8kVQJsg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/black-bean-burger-with-mashed-avocado--caramelized-onions-and-tomatoes-589977718-5ab84dcc30371300371106af.jpg");
-
-let newBurger1 = new menuItem('Chicken', 'all text', null, 700);
-let newBurger2 = new menuItem('Vegan', 'all text', null, 500);
-
-var menu = new Array(firstBurger,secondBurger,thirdBurger);
-inputer(menu);
 */
-/*let burger1 = document.getElementById('b1');
-let burgerPic = firstBurger.onload();
-burgerPic.height = "300";
-burgerPic.width = "300";
-burger1.appendChild(burgerPic);
-x.appendChild(t);
-let txt = document.createTextNode(firstBurger.burger());
-burger1.appendChild(txt);
-
-let burger2 = document.getElementById('b2');
-let burgerPic2 = secondBurger.onload();
-burgerPic2.height = "300";
-burgerPic2.width = "300";
-burger2.appendChild(burgerPic2);
-let txt2 = document.createTextNode(secondBurger.burger());
-burger2.appendChild(txt2);
-
-let burger3 = document.getElementById('b3');
-let burgerPic3 = thirdBurger.onload();
-burgerPic3.height = "300";
-burgerPic3.width = "300";
-burger3.appendChild(burgerPic3);
-let txt3 = document.createTextNode(thirdBurger.burger());
-burger3.appendChild(txt3);*/
-
-
-//Not Loop
-/*HTML
-<div id="myID">
-    <h1>Välj en burgare:</h1>
-    <li id='b1'></li>
-</div>
-document.getElementById("b1").innerHTML = firstBurger.burger();
-document.getElementById("b2").innerHTML = secondBurger.burger();
-document.getElementById("b3").innerHTML = thirdBurger.burger();
-document.getElementById("b4").innerHTML = newBurger1.burger();
-document.getElementById("b5").innerHTML = newBurger2.burger();*/
-
-
-// LOOP
-/*HTML
-<div id="myID">
-    <h1>Välj en burgare:</h1>
-</div>*/
-
-/*var menu = new Array(firstBurger, secondBurger, thirdBurger, newBurger1, newBurger2);
-
-
-for (let i = 0; i < 1; i++){
-var x = document.createElement("LI");
-var t = document.createTextNode( menu[i].burger());
-x.appendChild(t);
-document.getElementById("b1").appendChild(x);
-}*/
 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,45 @@
-new Vue({
+var select = new Vue({
   el: '#burgerSelect',
   data: {
-  	burgers
+  	burgers,
+  	checked: []
+  },
+  methods: {
+
   }
+})
+
+
+var custInfo = new Vue({
+	el: '#customerInfo',
+	data: {
+		name: '',
+		email: '',
+		street: '',
+		number: '',
+		picked: ''
+	}
+})
+
+var confirmBtn = new Vue({
+	el: '#confirm',
+	data: {
+	custBurger: [],	
+
+	custName: '',
+	custMail: '',
+	custStreet: '',
+	custNum:    '',
+	custGender: ''
+	},
+	methods: {
+		getOrder: function(){
+			this.custBurger = select.checked;
+			this.custName = custInfo.name;
+			this.custMail = custInfo.email;
+			this.custStreet = custInfo.street;
+			this.custNum = custInfo.number;
+			this.custGender = custInfo.picked;			
+	}
+}
 })
