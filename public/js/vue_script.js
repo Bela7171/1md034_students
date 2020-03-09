2ccb5b7ad6c8f8def7785909eaaede790a33ec77
Bela7171 <benny.lam.7171@student.uu.se>
2020-03-09 10:53:51 +0100

    Task 5-3 completed

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,101 +1,65 @@
-var select = new Vue({
-  el: '#burgerSelect',
-  data: {
-  	burgers,
-  	checked: []
-  },
-  methods: {
-
-  }
-})
-
-
-var custInfo = new Vue({
-	el: '#customerInfo',
-	data: {
-		name: '',
-		email: '',
-		picked: ''
-	}
-})
-
-var confirmBtn = new Vue({
-	el: '#confirm',
-	data: {
-	custBurger: [],	
-
-	custName: '',
-	custMail: '',
-	custGender: ''
-	},
-	methods: {
-		getOrder: function(){
-			this.custBurger = select.checked;
-			this.custName = custInfo.name;
-			this.custMail = custInfo.email;
-			this.custGender = custInfo.picked;			
-	}
-}
-})
-
-/*For task 5*/
-/* jslint es6:true, indent: 2 */
-/* global Vue, io */
-/* exported vm */
 'use strict';
 const socket = io();
 
-/* eslint-disable-next-line no-unused-vars */
-const vm = new Vue({
-  el: '#dots',
-  data: {
-    orders: {},
-  },
-  created: function() {
-    /* When the page is loaded, get the current orders stored on the server.
-     * (the server's code is in app.js) */
-    socket.on('initialize', function(data) {
-      this.orders = data.orders;
-    }.bind(this));
+const vm = new Vue ({
+	el:'#app',
+	data: {
+		burgers,
+		checked: [],
+		name: '',
+		email: '',
+		picked: '',
+		custBurger: [],	
+		custName: '',
+		custMail: '',
+		custGender: '',
 
-    /* Whenever an addOrder is emitted by a client (every open map.html is
-     * a client), the server responds with a currentQueue message (this is
-     * defined in app.js). The message's data payload is the entire updated
-     * order object. Here we define what the client should do with it.
-     * Spoiler: We replace the current local order object with the new one. */
-    socket.on('currentQueue', function(data) {
-      this.orders = data.orders;
-    }.bind(this));
-  },
-  methods: {
-    getNext: function() {
-      /* This function returns the next available key (order number) in
-       * the orders object, it works under the assumptions that all keys
-       * are integers. */
-      let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
-        return Math.max(last, next);
-      }, 0);
-      return lastOrder + 1;
-    },
-    addOrder: function(event) {
+		orders: {},
+		localOrder: {
+			orderId: '',
+			details: {x: 0, y: 0},
+			orderItems: []
+		},
+		numberOfOrder: 1,
+	},
+ methods: {
+ 	getOrder: function(){
+ 		this.custBurger = this.checked;
+ 		this.custName = this.name;
+ 		this.custMail = this.email;
+ 		this.custGender = this.picked;			
+ 	},
+ 	getNext: function() {
+ 	/*	This function returns the next available key (order number) in
+ 		* the orders object, it works under the assumptions that all keys
+ 		* are integers. */
+ 		return this.numberOfOrder++;
+ 	},
+ 	addOrder: function(event) {
       /* When you click in the map, a click event object is sent as parameter
        * to the function designated in v-on:click (i.e. this one).
        * The click event object contains among other things different
        * coordinates that we need when calculating where in the map the click
        * actually happened. */
-      let offset = {
-        x: event.currentTarget.getBoundingClientRect().left,
-        y: event.currentTarget.getBoundingClientRect().top,
-      };
-      socket.emit('addOrder', {
-        orderId: this.getNext(),
-        details: {
-          x: event.clientX - 10 - offset.x,
-          y: event.clientY - 10 - offset.y,
-        },
-        orderItems: ['Beans', 'Curry'],
-      });
-    },
-  },
-});
+       socket.emit('addOrder', {
+       	orderId: this.getNext(),
+       	details: this.localOrder.details,
+       	orderItems: this.checked,
+       });
+   },
+   /*Display order*/
+   displayOrder: function(event) {
 
+   	let offset = {
+   		x: event.currentTarget.getBoundingClientRect().left,
+   		y: event.currentTarget.getBoundingClientRect().top,
+   	};
+   	this.localOrder.orderId= 'T',
+   	this.localOrder.details= {
+   		x: event.clientX - 10 - offset.x,
+   		y: event.clientY - 10 - offset.y,
+   	},
+   	this.localOrder.orderItems= ['Beans', 'Curry'];
+   }
+}
+})
